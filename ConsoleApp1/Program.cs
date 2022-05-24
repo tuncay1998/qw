e2c50768e28244da0a9585dccbdca706c0b604c1
Tunjay J. Nasibli <agshin@code.local>
2022-05-24 13:39:45 +0400

    mellim 10 bal yaz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, 5, 7, 8, 9 };
+            int n = 7;
+            var index = -1;
+            int min = 0;
+            int max = numbers.Length - 1;
+
+            while (min < max)
+            {
+                int mid = (max - min) / 2;
+
+                if (numbers[mid] == n)
+                {
+                    index = mid;
+                    break;
+                }
+
+                else if (n < numbers[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+
+            }
+            Console.WriteLine(index);
+
+        }
+    }
+}
+
